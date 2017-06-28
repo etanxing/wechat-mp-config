d32070ad68775c120499337d25c71b0f55244066
William XING <yxing@deloitte.com.au>
2017-06-28 11:11:31 +0930

    access-token and ticket fetched

--- app.js
+++ app.js
@@ -1,10 +1,77 @@
+const qs = require('qs')
 const express = require('express')
+const axios = require('axios')
 const app = express()
 
-app.get('/', function (req, res) {
+let accessToken, expiresIn, ticket
+
+const getAccessToken = () => {
+  let queryParams = {
+    'grant_type': 'client_credential',
+    'appid': 'wx23427b4112bc5836',
+    'secret': 'ccfadcd8cb3bae9da2bdd49ccd29244e'
+  };
+
+  let wxGetAccessTokenBaseUrl = 'https://api.weixin.qq.com/cgi-bin/token?'+qs.stringify(queryParams);
+
+  return axios.get(wxGetAccessTokenBaseUrl)
+};
+
+const getTicket = () => {
+  let queryParams = {
+    'access_token': accessToken,
+    'type': 'jsapi'
+  };
+
+  let wxGetTicketBaseUrl = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?'+qs.stringify(queryParams);
+
+  return axios.get(wxGetTicketBaseUrl)
+};
+
+
+getAccessToken()
+.then(({data}) => {
+  accessToken = data['access_token'];
+  expiresIn = data['expires_in'];
+  return getTicket()
+})
+.then(({data}) => {
+  ticket = data['ticket']
+  return ticket
+})
+.then((tk) => {
+  console.log(accessToken, ticket, expiresIn);
+
+  setInterval(() => {
+    getAccessToken()
+    .then(({data}) => {
+      accessToken = data['access_token'];
+      return getTicket()
+    })
+    .then(({data}) => {
+      ticket = data['ticket']
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }, (expiresIn - 200)*1000);
+
+  app.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
+  })
+})
+.catch((error) => {
+  console.log(error);
+})
+
+app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.get('/getconfig', (req, res) => {
+  if (accessToken) {
+
+  } else {
+
+  }
 })
